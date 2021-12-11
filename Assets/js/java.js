5c4b998ebcd358ff89225086cd57eb3770eee0b3
YooNazZ <Jonaserik120603@gmail.com>
2021-12-11 02:04:19 +0100

    Updated

--- Assets/js/java.js
+++ Assets/js/java.js
@@ -94,3 +94,23 @@ function beregnFedtfri() {
 	c = a * b * 34;
 	document.getElementById("cer1").innerHTML = c + " Kg.";
 }
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
